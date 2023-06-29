e93f300d8f028b48ba3403982a0556a3bb8c6b2b
Dmytro Kulakov <kulakovdmitr@gmail.com>
2023-06-30 02:03:59 +0300

    dont install chrome

--- test.sh
+++ test.sh
@@ -39,24 +39,24 @@ fi
 #     exit 1
 # fi
 
-echo "Installing Chrome.."
-if [[ $(getconf LONG_BIT) = "64" ]]
-then
-    echo "64bit Detected" &&
-    echo "Installing Google Chrome" &&
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
-    sudo dpkg -i google-chrome-stable_current_amd64.deb &&
-    rm -f google-chrome-stable_current_amd64.deb
-else
-    echo "32bit Detected" &&
-    echo "Installing Google Chrome" &&
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb &&
-    sudo dpkg -i google-chrome-stable_current_i386.deb &&
-    rm -f google-chrome-stable_current_i386.deb
-fi
+# echo "Installing Chrome.."
+# if [[ $(getconf LONG_BIT) = "64" ]]
+# then
+#     echo "64bit Detected" &&
+#     echo "Installing Google Chrome" &&
+#     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
+#     sudo dpkg -i google-chrome-stable_current_amd64.deb &&
+#     rm -f google-chrome-stable_current_amd64.deb
+# else
+#     echo "32bit Detected" &&
+#     echo "Installing Google Chrome" &&
+#     wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb &&
+#     sudo dpkg -i google-chrome-stable_current_i386.deb &&
+#     rm -f google-chrome-stable_current_i386.deb
+# fi
 
 ## Installing clean up
-sudo apt install lsof
+apt install lsof
 
 echo "Cloning React Shared Tests Repo.."
 mkdir $REACT_TESTS_FOLDER
