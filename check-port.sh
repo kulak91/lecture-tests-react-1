c1265d2583f906c674602502cd14f39156129cae
Dmytro Kulakov <kulakovdmitr@gmail.com>
2023-07-12 13:37:58 +0300

    update check port

--- check-port.sh
+++ check-port.sh
@@ -3,34 +3,49 @@
 FILENAME=/proc/net/netstat
 
 PORT=${1:-3000}
+host="localhost"
+start_port=$PORT
+end_port=3100
 EMPTY_PORT=$?
 
-function check_port {
-
-case `cat "$FILENAME" | grep "$PORT" >/dev/null; echo $?` in
-  0)
-    # echo "Port \"$PORT\" is taken. Incrementing.."
-    ;;
-  1)
-    # echo "Port \"$PORT\" is free. Proceeding.."
-    EMPTY_PORT=$PORT
-    echo "$PORT"
-    ;;
-  *)
-  echo "error"
-  exit 0
-    ;;
-esac
-
-}
-
-
-while [ $EMPTY_PORT == 0 ]
+for ((port=start_port; port<=end_port; port++))
 do
-# echo $PORT
-check_port
-((PORT++))
+    (</dev/tcp/"$host"/"$port") >/dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo "$port"
+        exit 0
+    # else
+        # echo "$port is in use"
+    fi
 done
+
+
+# function check_port {
+
+# case `cat "$FILENAME" | grep "$PORT" >/dev/null; echo $?` in
+#   0)
+#     # echo "Port \"$PORT\" is taken. Incrementing.."
+#     ;;
+#   1)
+#     # echo "Port \"$PORT\" is free. Proceeding.."
+#     EMPTY_PORT=$PORT
+#     echo "$PORT"
+#     ;;
+#   *)
+#   echo "error"
+#   exit 0
+#     ;;
+# esac
+
+# }
+
+
+# while [ $EMPTY_PORT == 0 ]
+# do
+# # echo $PORT
+# check_port
+# ((PORT++))
+# done
 
 
 
